0f4edb3bf9dc914f35c510f54e2739b2be6d4cac
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-01 13:03:50 +0100

    Műveletek tesztelve, Routes.php működik

--- src/Routes.php
+++ src/Routes.php
@@ -1,7 +1,9 @@
 <?php
 
+use Doggo\Database\Ertekeles;
 use Doggo\Database\Felhasznalok;
 use Doggo\Database\Helyszin;
+use Doggo\Database\Komment;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -78,7 +80,7 @@ return function(Slim\App $app) {
         $app->get('/felhasznalo', function(Request $request, Response $response) {
             $felhasznalo = Felhasznalok::all();
             $kimenet = $felhasznalo->toJson();
-            
+
             $response->getBody()->write($kimenet);
             return $response->withHeader('Content-Type', 'application/json');
         });
@@ -137,6 +139,142 @@ return function(Slim\App $app) {
                 $felhasznalo->fill($input);
                 $felhasznalo->save();
                 $response->getBody()->write($felhasznalo->toJson());
+                return $response
+                    ->withHeader('Content-Type', 'application/json')
+                    ->withStatus(200);
+            });
+
+        //Kommentek
+        $app->get('/komment', function(Request $request, Response $response) {
+            $komment = Komment::all();
+            $kimenet = $komment->toJson();
+            
+            $response->getBody()->write($kimenet);
+            return $response->withHeader('Content-Type', 'application/json');
+        });
+    
+        $app->post('/komment', function(Request $request, Response $response) {
+            $input = json_decode($request->getBody(), true);
+            $komment = Komment::create($input);
+    
+            $kimenet = $komment->toJson();
+            
+            $response->getBody()->write($kimenet);
+            return $response
+                ->withStatus(201)
+                ->withHeader('Content-Type', 'application/json');
+        });
+    
+        $app->delete('/komment/{id}',
+            function (Request $request, Response $response, array $args) {
+                if (!is_numeric($args['id']) || $args['id'] <= 0) {
+                    $ki = json_encode(['error' => 'Az ID pozitív egész szám kell legyen!']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(400);
+                }
+                $komment = Komment::find($args['id']);
+                if ($komment === null) {
+                    $ki = json_encode(['error' => 'Nincs ilyen ID-jűHelyszin']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(404);
+                }
+                $komment->delete();
+                return $response
+                    ->withStatus(204);
+            });
+    
+            $app->put('/komment/{id}', function (Request $request, Response $response, array $args) {
+                if (!is_numeric($args['id']) || $args['id'] <= 0) {
+                    $ki = json_encode(['error' => 'Az ID pozitív egész szám kell legyen!']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(400);
+                }
+                $komment = Komment::find($args['id']);
+                if ($komment === null) {
+                    $ki = json_encode(['error' => 'Nincs ilyen ID-jűHelyszin']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(404);
+                }
+                $input = json_decode($request->getBody(), true);
+                $komment->fill($input);
+                $komment->save();
+                $response->getBody()->write($komment->toJson());
+                return $response
+                    ->withHeader('Content-Type', 'application/json')
+                    ->withStatus(200);
+            });
+
+        //Értékelések
+        $app->get('/ertekeles', function(Request $request, Response $response) {
+            $ertekeles = Ertekeles::all();
+            $kimenet = $ertekeles->toJson();
+            
+            $response->getBody()->write($kimenet);
+            return $response->withHeader('Content-Type', 'application/json');
+        });
+    
+        $app->post('/ertekeles', function(Request $request, Response $response) {
+            $input = json_decode($request->getBody(), true);
+            $ertekeles = Ertekeles::create($input);
+    
+            $kimenet = $ertekeles->toJson();
+            
+            $response->getBody()->write($kimenet);
+            return $response
+                ->withStatus(201)
+                ->withHeader('Content-Type', 'application/json');
+        });
+    
+        $app->delete('/ertekeles/{id}',
+            function (Request $request, Response $response, array $args) {
+                if (!is_numeric($args['id']) || $args['id'] <= 0) {
+                    $ki = json_encode(['error' => 'Az ID pozitív egész szám kell legyen!']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(400);
+                }
+                $ertekeles = Ertekeles::find($args['id']);
+                if ($ertekeles === null) {
+                    $ki = json_encode(['error' => 'Nincs ilyen ID-jűHelyszin']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(404);
+                }
+                $ertekeles->delete();
+                return $response
+                    ->withStatus(204);
+            });
+    
+            $app->put('/ertekeles/{id}', function (Request $request, Response $response, array $args) {
+                if (!is_numeric($args['id']) || $args['id'] <= 0) {
+                    $ki = json_encode(['error' => 'Az ID pozitív egész szám kell legyen!']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(400);
+                }
+                $ertekeles = Ertekeles::find($args['id']);
+                if ($ertekeles === null) {
+                    $ki = json_encode(['error' => 'Nincs ilyen ID-jűHelyszin']);
+                    $response->getBody()->write($ki);
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus(404);
+                }
+                $input = json_decode($request->getBody(), true);
+                $ertekeles->fill($input);
+                $ertekeles->save();
+                $response->getBody()->write($ertekeles->toJson());
                 return $response
                     ->withHeader('Content-Type', 'application/json')
                     ->withStatus(200);
